a51890f8574afb04ee50c0dcd1925c4d5a713c75
陳力睿 <jerrylj.chen@gmail.com>
2019-09-19 21:07:11 +0800

    prepare the data for training

--- script.js
+++ script.js
@@ -53,3 +53,50 @@ async function getData() {
   
     return model;
   }
+
+  /**
+ * Convert the input data to tensors that we can use for machine 
+ * learning. We will also do the important best practices of _shuffling_
+ * the data and _normalizing_ the data
+ * MPG on the y-axis.
+ */
+function convertToTensor(data) {
+  // Wrapping these calculations in a tidy will dispose any 
+  // intermediate tensors.
+  
+  return tf.tidy(() => {
+    // Step 1. Shuffle the data, 幫助每批有不同資料變異性   
+    tf.util.shuffle(data);
+
+    // Step 2. Convert data to Tensor
+    // 拿到２個array
+    const inputs = data.map(d => d.horsepower)
+    const labels = data.map(d => d.mpg);
+
+    // 轉換array到2d tensor, 其形狀為[num_examples, num_features_per_example]
+    // 有input.length 範例，且每個範例有１個input特徵 (馬力)
+    const inputTensor = tf.tensor2d(inputs, [inputs.length, 1]);
+    const labelTensor = tf.tensor2d(labels, [labels.length, 1]);
+
+    //Step 3. Normalize the data to the range 0 - 1 using min-max scaling
+    // tensorflow 設計處理之數字不會太大，所以轉換為 0 - 1 / -1 - 1 最棒
+    // 保持好習慣，在訓練資料前先常態化
+    const inputMax = inputTensor.max();
+    const inputMin = inputTensor.min();  
+    const labelMax = labelTensor.max();
+    const labelMin = labelTensor.min();
+
+    const normalizedInputs = inputTensor.sub(inputMin).div(inputMax.sub(inputMin));
+    const normalizedLabels = labelTensor.sub(labelMin).div(labelMax.sub(labelMin));
+
+    return {
+      inputs: normalizedInputs,
+      labels: normalizedLabels,
+      // Return the min/max bounds so we can use them later.
+      inputMax,
+      inputMin,
+      labelMax,
+      labelMin,
+    }
+  });  
+}
